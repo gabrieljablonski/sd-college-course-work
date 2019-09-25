3d8c206acd2d17c17b987977713be620d1da3820
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-09-24 21:21:42 -0300

    updated spid structure
    marshaling stuff

--- src/spidTCPServer/entities/spid.go
+++ src/spidTCPServer/entities/spid.go
@@ -1,12 +1,57 @@
 package entities
 
 import (
+	"encoding/json"
+	"github.com/google/uuid"
+	eh "main/errorHandling"
 	"main/gps"
+	"time"
 )
 
 type Spid struct {
-	ID            string `json:"id"`
+	ID            uuid.UUID          `json:"id"`
+	BatteryLevel  uint8              `json:"battery_level"`
+	LockState     string             `json:"lock_state"`
 	Location      gps.GlobalPosition `json:"location"`
-	CurrentUserID string `json:"current_user_id"`
-	BatteryLevel  float32 `json:"battery_level"`
+	LastUpdated   time.Time          `json:"last_updated"`
+	CurrentUserID string             `json:"current_user_id"`
+}
+
+type Spids struct {
+	Spids map[uuid.UUID]Spid `json:"spids"`
+}
+
+func NewSpid() Spid {
+	return Spid{
+		uuid.New(),
+		100,
+		"locked",
+		gps.NullPosition(),
+		time.Unix(0,0),
+		"",
+	}
+}
+
+func (s Spid) Marshal() []byte {
+	um, err := json.Marshal(u)
+	eh.HandleFatal(err)
+	return um
+}
+
+func MarshalSpids(spids Spids) []byte {
+	marshaledSpids, err := json.MarshalIndent(users, "", "    ")
+	eh.HandleFatal(err)
+	return marshaledSpids
+}
+
+func UnmarshalSpids(marshaledSpids []byte) Spids {
+	var spids Spids
+	err := json.Unmarshal(marshaledSpids, &spids)
+	eh.HandleFatal(err)
+	return spids
+}
+
+func (s Spid) UpdateLocation(position gps.GlobalPosition) {
+	s.Location = position
+	s.LastUpdated = time.Now()
 }
