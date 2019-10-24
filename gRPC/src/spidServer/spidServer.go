47e1771086e3ed98291457eb504ddf373b37ac39
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-10-23 21:38:24 -0300

    added server stub

---
+++ gRPC/src/spidServer/spidServer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"spidServer/requestHandling"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal("failed", err)
+	}
+	s := grpc.NewServer()
+	requestHandling.RegisterSpidHandlerServer(s, &requestHandling.Handler{})
+	log.Print("serving...")
+	err = s.Serve(listener)
+	if err != nil {
+		log.Fatal("failed", err)
+	}
+	log.Print("served...")
+}
