964aff118c16f1bdc44656098a44b90106f718c0
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-10-26 21:21:52 -0300

    added spid grpc wrapper handlers

--- gRPC/src/spidServer/requestHandling/grpcWrapper/spidWrapper.go
+++ gRPC/src/spidServer/requestHandling/grpcWrapper/spidWrapper.go
@@ -2,25 +2,86 @@ package grpcWrapper
 
 import (
 	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"spidServer/entities"
+	"spidServer/gps"
 	pb "spidServer/requestHandling/grpcWrapper/spidProtoBuffers"
 )
 
+func (w *Wrapper) querySpid(spidID string) (spid entities.Spid, err error) {
+	id, err := uuid.Parse(spidID)
+	if err != nil {
+		return spid, fmt.Errorf("invalid user id: %s", err)
+	}
+	return w.Handler.Manager.QuerySpid(id)
+}
+
 func (w *Wrapper) GetSpidInfo(ctx context.Context, request *pb.GetSpidRequest) (*pb.GetSpidResponse, error) {
-	return &pb.GetSpidResponse{}, nil
+	spid, err :=  w.querySpid(request.SpidID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get spid info: %s", err)
+	}
+	return &pb.GetSpidResponse{
+		Message: "Spid queried successfully.",
+		Spid:    spid.ToProtoBufferEntity(),
+	}, nil
 }
 
 func (w *Wrapper) RegisterSpid(ctx context.Context, request *pb.RegisterSpidRequest) (*pb.RegisterSpidResponse, error) {
-	return &pb.RegisterSpidResponse{}, nil
+	spid, err :=  w.Handler.Manager.RegisterSpid()
+	if err != nil {
+		return nil, fmt.Errorf("failed to register spid")
+	}
+	return &pb.RegisterSpidResponse{
+		Message: "Spid registered successfully",
+		Spid:    spid.ToProtoBufferEntity(),
+	}, nil
 }
 
 func (w *Wrapper) UpdateBatteryInfo(ctx context.Context, request *pb.UpdateBatteryRequest) (*pb.UpdateBatteryResponse, error) {
-	return &pb.UpdateBatteryResponse{}, nil
+	spid, err := w.querySpid(request.SpidID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update spid battery info: %s", err)
+	}
+	spid.BatteryLevel = request.BatteryLevel
+	err = w.Handler.Manager.UpdateSpid(spid)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update spid battery info: %s", err)
+	}
+	return &pb.UpdateBatteryResponse{
+		Message: "Battery info updated successfully.",
+		Spid:    spid.ToProtoBufferEntity(),
+	}, nil
 }
 
 func (w *Wrapper) UpdateSpidLocation(ctx context.Context, request *pb.UpdateSpidLocationRequest) (*pb.UpdateSpidLocationResponse, error) {
-	return &pb.UpdateSpidLocationResponse{}, nil
+	spid, err := w.querySpid(request.SpidID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update spid location: %s", err)
+	}
+	spid.Location = gps.FromProtoBufferEntity(request.Location)
+	err = w.Handler.Manager.UpdateSpid(spid)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update spid location: %s", err)
+	}
+	return &pb.UpdateSpidLocationResponse{
+		Message: "Spid location updated successfully.",
+		Spid:    spid.ToProtoBufferEntity(),
+	}, nil
 }
 
 func (w *Wrapper) DeleteSpid(ctx context.Context, request *pb.DeleteSpidRequest) (*pb.DeleteSpidResponse, error) {
-	return &pb.DeleteSpidResponse{}, nil
+	spid, err := w.querySpid(request.SpidID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete spid: %s", err)
+	}
+	err = w.Handler.Manager.DeleteSpid(spid)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete spid: %s", err)
+	}
+	return &pb.DeleteSpidResponse{
+		Message: "Spid deleted successfully.",
+		Spid:    spid.ToProtoBufferEntity(),
+	}, nil
 }
