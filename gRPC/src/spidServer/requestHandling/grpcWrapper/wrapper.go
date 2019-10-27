d081593dfe9fdb023ffa1a0283f37f04eda71097
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-10-26 21:23:15 -0300

    Update wrapper.go

--- gRPC/src/spidServer/requestHandling/grpcWrapper/wrapper.go
+++ gRPC/src/spidServer/requestHandling/grpcWrapper/wrapper.go
@@ -1,10 +1,12 @@
 package grpcWrapper
 
 import (
+	"spidServer/requestHandling"
 	pb "spidServer/requestHandling/grpcWrapper/spidProtoBuffers"
 )
 
 type Wrapper struct {
+	Handler requestHandling.Handler
 	pb.SpidHandlerServer
 	pb.UserHandlerServer
 }
