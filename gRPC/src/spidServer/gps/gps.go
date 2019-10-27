73d65bd70978d58eaa5134f46adbaccf29e0010a
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-10-26 21:20:41 -0300

    added from protobuffer gps

--- gRPC/src/spidServer/gps/gps.go
+++ gRPC/src/spidServer/gps/gps.go
@@ -12,6 +12,13 @@ func NullPosition() GlobalPosition {
 	return GlobalPosition{0, 0}
 }
 
+func FromProtoBufferEntity(position *spidProtoBuffers.GlobalPosition) GlobalPosition {
+	return GlobalPosition{
+		Latitude:  position.Latitude,
+		Longitude: position.Longitude,
+	}
+}
+
 func (p GlobalPosition) ToProtoBufferEntity() *spidProtoBuffers.GlobalPosition {
 	return &spidProtoBuffers.GlobalPosition{
 		Latitude:  p.Latitude,
