0e523726c4678354e1656847dab8c5b696d1dace
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-10-27 22:20:41 -0300

    implemented map to server functions
    - map by entity ID
    - map by global position

--- gRPC/src/spidServer/requestHandling/grpcHandler.go
+++ gRPC/src/spidServer/requestHandling/grpcHandler.go
@@ -2,11 +2,14 @@ package requestHandling
 
 import (
 	"spidServer/db"
+	"spidServer/grpcServer"
 	pb "spidServer/requestHandling/protoBuffers"
 )
 
 type Handler struct {
 	DBManager db.Manager
+	IPTable	              []grpcServer.IP
+
 	pb.SpidHandlerServer
 	pb.UserHandlerServer
 }
