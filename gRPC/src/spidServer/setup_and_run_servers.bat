993724c8aa6064edfa3b16e920b20dc08289b111
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2019-11-11 14:04:48 -0300

    remove duplicate folder

--- gRPC/src/spidServer/setup_and_run_servers.bat
+++
@@ -1,9 +0,0 @@
-:: %1 -> base delta
-:: %2 -> servers base port
-:: %3 -> server mapper address
-:: %4 -> server mapper port
-
-go build spidServer.go
-create_server_instances.py -d %1 -p %2
-start server_mapper.py -d %1
-run_servers.py -a %3 -p %4
